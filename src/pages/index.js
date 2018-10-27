ff7fd97b555b1dc34b6d5eabc9a3a8addf190b46
Harsh P Bajaj <harsh.rocks@hotmail.com>
2018-10-27 12:30:46 -0500

    Change README and project files

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>I'm Gabriel Garcia.</p>
+    <p>I'm from Mexico</p>
+    <Link to="/page-2/">Myself</Link>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Frontend Developer</h1>
+    <p>I enjoy building usable and beautiful interfaces</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
