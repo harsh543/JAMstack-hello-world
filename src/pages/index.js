a264111949f51ade0a38b1a399ffe8e8c453feab
Harsh P Bajaj <harsh.rocks@hotmail.com>
2018-11-09 17:08:33 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,12 @@ const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>I'm Harsh Bajaj.</p>
-    <p>I'm from India</p>
-    <Link to="/page-2/">Myself</Link>
+    <p>I'm a Cybersecurity researcher at University of Illinois </p>
+    <p>I keep coding here as well: https://www.hackerrank.com/harshpbajaj </p>
+    <p> THe last hackathon I am proud of was : https://hackuiowa.hackerearth.com/sprints/hackuiowa-1/dashboard/fbab7af/submission/</p>
+  <p>ANd I contribute to Open Source as well : https://www.owasp.org/index.php/OWASP_Serverless_Top_10_Project . 
+& http://w3af.org/ </p>  
+  <Link to="/page-2/">Myself</Link>
   </div>
 )
 
